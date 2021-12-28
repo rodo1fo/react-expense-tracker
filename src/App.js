7ff33e9af42fa9322245dea48038a0970c7031a5
Rodolfo L <rodolfo.nat@gmail.com>
2021-12-28 17:47:16 -0300

    add expense ExpensesList component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -22,10 +23,10 @@ const App = () => {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ];
+  ]);
 
   const addExpenseHandler = expenseData => {
-    console.log(expenseData);
+    setExpenses(prevState => [expenseData, ...prevState]);
   };
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem/ExpenseItem';
+
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter/ExpensesFilter';
+import ExpensesList from './ExpensesList/ExpensesList';
+
 import './Expenses.css';
 
 const Expenses = props => {
@@ -11,14 +13,15 @@ const Expenses = props => {
     setFilteredYear(enteredDate);
   };
 
+  const filteredExpenses = props.items.filter(
+    expense => new Date(expense.date).getFullYear() === parseInt(filteredYear)
+  );
+
   return (
     <div>
-      <ExpensesFilter selectedYear={filteredYear} onChangeFilter={selectDateHandler} />
       <Card className="expenses">
-        <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-        <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+        <ExpensesFilter selectedYear={filteredYear} onChangeFilter={selectDateHandler} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
